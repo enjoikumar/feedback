ee2fd851103c3ea5dec48e4db0d86e1f43605681
Kamal Kumar <kamalkumar@dellt3500-26.sblm.local>
2018-04-24 13:47:51 -0400

    set up google auth

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ bye: 'buddy' });
-});
+passport.use(new GoogleStrategy({ //creates new instance of google passport(login)
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret
+	})
+); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(3000);
+
