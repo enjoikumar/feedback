de9394e0d5753ed55390cf2a567cdbceacce3337
Kamal Kumar <kamalkumar@dellt3500-26.sblm.local>
2018-04-24 16:50:57 -0400

    set up mongodb

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
 require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 require('./routes/authRoutes')(app);
+
 
 const PORT = process.env.PORT || 3000;
 console.log("ʕノ•ᴥ•ʔノ ︵ ┻━┻")
