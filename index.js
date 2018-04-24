00831086ccc7d67b81fe0dbc00196f960d0ae6f7
Kamal Kumar <kamalkumar@dellt3500-26.sblm.local>
2018-04-24 15:01:46 -0400

    initial route works

--- index.js
+++ index.js
@@ -4,13 +4,34 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 
 const app = express();
-
-passport.use(new GoogleStrategy({ //creates new instance of google passport(login)
+//creates new instance of google passport(login)
+passport.use(
+	new GoogleStrategy(
+	{       
 		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback'
+	}, 
+		(accessToken, refreshToken, profile, done) => {
+			console.log('access token', accessToken);
+			console.log('refresh token', refreshToken);
+			console.log('profile:', profile);
+		}
+	)
+);
+
+app.get(
+	'/auth/google', 
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
 	})
-); 
+);
+
+app.get('/auth/google/callback', 
+	passport.authenticate('google')
+);
 
 const PORT = process.env.PORT || 3000;
+console.log("ʕノ•ᴥ•ʔノ ︵ ┻━┻")
 app.listen(3000);
 
