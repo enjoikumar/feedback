c7b2e7d813f044270e47e739135330422efab0dc
Kamal Kumar <kamalkumar@dellt3500-26.sblm.local>
2018-04-25 10:41:37 -0400

    no duplicate users

--- services/passport.js
+++ services/passport.js
@@ -1,19 +1,30 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 //creates new instance of google passport(login)
 passport.use(
 	new GoogleStrategy(
-	{       
-		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback'
-	}, 
+		{       
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		}, 
 		(accessToken, refreshToken, profile, done) => {
-			console.log('access token', accessToken);
-			console.log('refresh token', refreshToken);
-			console.log('profile:', profile);
+			User.findOne({ googleId : profile.id })
+				.then((existingUser) => {
+					if (existingUser) {
+						//we already have a record with the given profile ID
+						done(null, existingUser);
+					} else{
+						//we dont have a user record, make a new record
+						new User ({ googleId: profile.id }).save()
+							.then(user => done(null, user));
+					}
+				});			
 		}
 	)
 );
