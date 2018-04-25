8ee96b72882df92f6918f76e98532c1b0d8d5f5e
Kamal Kumar <kamalkumar@dellt3500-26.sblm.local>
2018-04-25 15:15:55 -0400

    changes to config

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//DEV.js
+
+// mongodb://kumar:password@ds257589.mlab.com:57589/emaily-p-rod
+
+// production creds
+// google client: 281153007357-qlr17gg7d9fe6d6lhft2u7jv808c0hc8.apps.googleusercontent.com
+// google client secret: KXM99g4kOIS3En8kwW1FCy7R
